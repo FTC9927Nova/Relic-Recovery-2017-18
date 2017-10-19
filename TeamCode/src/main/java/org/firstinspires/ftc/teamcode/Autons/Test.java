9a5d26e7a9c7915bae1e5b7e7cfd12fc9041e5fa
Ethan Pereira <ethanpereira707@gmail.com>
2017-10-19 16:33:10 -0400

    New Changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/Test.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Autons;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Ethan Pereira on 10/19/2017.
+ */
+
+public class Test extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo jewl = hardwareMap.servo.get("jewl");
+        if (opModeIsActive()){
+            if (jewl.getPosition() == 1.0){
+                jewl.setPosition(0.0);
+            }
+        }
+    }
+}
