95ac95896f67cb305478b627180e549d4b0c2fea
Ethan Pereira <ethanpereira707@gmail.com>
2017-10-20 16:54:07 -0400

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/Test.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.Autons;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -8,16 +11,18 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by Ethan Pereira on 10/19/2017.
  */
 @TeleOp(name = "JewlArmTest")
-public class Test extends LinearOpMode {
+public class Test extends OpMode {
+    Servo jewl;
+    @Override
+    public void init() {
+        jewl = hardwareMap.servo.get("jewl");
+
+    }
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        Servo jewl = hardwareMap.servo.get("jewl");
-        waitForStart();
-        while (opModeIsActive()){
-            telemetry.addData("Pos", jewl.getPosition());
-            telemetry.addData("Direction", jewl.getDirection());
-            telemetry.update();
-        }
+    public void loop() {
+        telemetry.addData("Pos", jewl.getPosition());
+        telemetry.addData("Direction", jewl.getDirection());
+        Log.i("DataLogs", String.valueOf(jewl.getPosition()));
     }
 }
