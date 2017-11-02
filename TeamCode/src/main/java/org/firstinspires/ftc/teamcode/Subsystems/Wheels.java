0da8acd270018c17ea676254bbfb43bb80670c96
Sumanth <skondapalli02@gmail.com>
2017-11-02 19:29:49 -0400

    WHEELZZZZ

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Wheels.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+import org.firstinspires.ftc.teamcode.Util.*;
+import org.firstinspires.ftc.teamcode.Subsystems.SubsystemTemplate;
+import com.qualcomm.robotcore.hardware.*;
+
+/**
+ * Created by Sumanth on 11/2/17.
+ */
+
+public class Wheels implements SubsystemTemplate{
+
+    private DcMotor leftWheels, rightWheels;
+
+
+    public Wheels(HardwareMap hardwareMap)
+    {
+
+        leftWheels = hardwareMap.dcMotor.get("leftIntake");
+        rightWheels = hardwareMap.dcMotor.get("rightIntake");
+
+        setMode(Mode.STOP_RESET);
+        leftWheels.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightWheels.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+    public enum Mode
+    {
+        ENCODERS,
+        SPEED,
+        STOP_RESET,
+        NOTHING
+    }
+
+    public void setMode(Mode d) {
+        DcMotor.RunMode runMode = null;
+
+        switch (d) {
+            case ENCODERS:
+                runMode = DcMotor.RunMode.RUN_TO_POSITION;
+                break;
+            case SPEED:
+                runMode = DcMotor.RunMode.RUN_USING_ENCODER;
+                break;
+            case STOP_RESET:
+                runMode = DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+                break;
+            case NOTHING:
+                runMode = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+                break;
+
+        }
+
+        leftWheels.setMode(runMode);
+        rightWheels.setMode(runMode);
+
+    }
+
+    public void intake(){
+
+        leftWheels.setPower(1);
+        rightWheels.setPower(1);
+
+    }
+    public void outtake(){
+
+        leftWheels.setPower(-1);
+        rightWheels.setPower(-1);
+
+    }
+
+    @Override
+    public String display() {
+
+        return null;
+
+    }
+}
