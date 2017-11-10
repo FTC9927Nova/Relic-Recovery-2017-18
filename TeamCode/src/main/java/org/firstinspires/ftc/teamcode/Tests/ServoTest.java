172df658f69c91429ece24af31b5859182bba41c
Ethan Pereira <ethanpereira707@gmail.com>
2017-11-10 09:59:56 -0500

    11/10/17

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ServoTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Ethan Pereira on 11/10/2017.
+ */
+
+public class ServoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
