5929e69aed8af3c85f2c333f0edea6eb33127a15
Sumanth <skondapalli02@gmail.com>
2018-04-25 14:41:58 -0400

    "PotCode "

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/RedCloseMGAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/RedCloseMGAuton.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain;
 import org.firstinspires.ftc.teamcode.Subsystems.FourBar;
+import org.firstinspires.ftc.teamcode.Subsystems.JewelArm;
 import org.firstinspires.ftc.teamcode.Subsystems.Robot;
 import org.firstinspires.ftc.teamcode.Util.Gyro;
 import org.firstinspires.ftc.teamcode.Util.VisionUtil;
@@ -17,7 +18,7 @@ import org.firstinspires.ftc.teamcode.Util.*;
 /**
  * Created by Sumanth Kondapalli on 11/16/2017.
  */
-@Autonomous(name = "RedCloseMGAuton")
+@Autonomous(name = "RedCloseMGY")
 public class RedCloseMGAuton extends LinearOpMode {
 
     Robot robot = new Robot();
@@ -29,13 +30,13 @@ public class RedCloseMGAuton extends LinearOpMode {
     double startTime = 0;
     String tester = "";
     VisionUtil visionUtil = new VisionUtil(this);
-
-    double firstAnlge;
+    double angle;
 
     double startEnc;
     double targetEnc;
     enum DriveState
     {
+        HIT_JEWEL,
         DRIVE_TO_POS,
         ROTATE_TO_GLYPH_PIT,
         GET_FIRST_GLYPH,
@@ -44,7 +45,15 @@ public class RedCloseMGAuton extends LinearOpMode {
         LEFT,
         RIGHT,
         CENTER,
+        ROTATE_DEG_2,
         STOP
+    }
+
+    enum JewelState
+    {
+        RED,
+        BLUE,
+        NONE,
     }
 
 
@@ -66,14 +75,24 @@ public class RedCloseMGAuton extends LinearOpMode {
         STOP
     }
 
+    enum ArmState
+    {
+        ARM_UP,
+        ARM_DOWN
+    }
+
+
+
     WheelState wheelState = WheelState.STOP;
     DriveState driveState = DriveState.DRIVE_TO_POS;
     DriveState firstDState = DriveState.CENTER;
     FourBarState fourBarState = FourBarState.STOP;
+    JewelState jewelState = JewelState.NONE;
+    ArmState armState = ArmState.ARM_DOWN;
 
     @Override
-    public void runOpMode() throws InterruptedException
-    {
+    public void runOpMode() throws InterruptedException {
+
         AutoTransitioner.transitionOnStop(this,"MainTeleNoPID");
 
         gyro.initGyro(hardwareMap);
@@ -82,22 +101,26 @@ public class RedCloseMGAuton extends LinearOpMode {
 
         reading = vision.readGraph2(hardwareMap);
         telemetry.addData("vision", reading);
+
         switch(reading)
         {
             case LEFT:
             {
                 firstDState = DriveState.LEFT;
+                angle = 165;
                 break;
             }
             case RIGHT:
             {
                 firstDState = DriveState.RIGHT;
+                angle = -165;
                 break;
 
             }
             case CENTER:
             {
                 firstDState = DriveState.CENTER;
+                angle = 180;
                 break;
             }
             default:
@@ -105,12 +128,60 @@ public class RedCloseMGAuton extends LinearOpMode {
                 firstDState = DriveState.CENTER;
             }
         }
-        telemetry.addData("first state", firstDState.toString());
-        telemetry.update();
+
         waitForStart();
-        while(opModeIsActive()){
-            if(gyro.getPitch()>10)
+        while(opModeIsActive())
+        {
+            if(gyro.getPitch()>7)
                 stop();
+
+//            switch (armState)
+//            {
+//                case ARM_DOWN:
+//                {
+//                    robot.jewelArm.arm2Down();
+//                    break;
+//                }
+//                case ARM_UP:
+//                {
+//                    robot.jewelArm.arm2Up();
+//                    break;
+//                }
+//            }
+////
+//            switch (jewelState)
+//            {
+//                case RED:
+//                {
+//                   driveState = DriveState.DRIVE_TO_POS;
+//                   break;
+//                }
+//                case BLUE:
+//                {
+//                    if(robot.driveTrain.setMoveDist(-4))
+//                    {
+//                        driveState = DriveState.DRIVE_TO_POS;
+//                    }
+//                    break;
+//                }
+//                case NONE:
+//                {
+//                    if(robot.jewelArm.getColor2().equals(JewelArm.ColorDetected.RED))
+//                    {
+//                        jewelState = JewelState.RED;
+//                    }
+//                    else if(robot.jewelArm.getColor2().equals(JewelArm.ColorDetected.BLUE))
+//                    {
+//                        jewelState = JewelState.BLUE;
+//                    }
+//                    break;
+//                }
+//            }
+//
+//            armState = ArmState.ARM_DOWN;
+
+
+
             switch (driveState)
             {
                 case DRIVE_TO_POS:
@@ -142,7 +213,9 @@ public class RedCloseMGAuton extends LinearOpMode {
                         robot.driveTrain.setDrive(DriveTrain.Drive.STOP_RESET);
                         robot.driveTrain.stop();
 
-                        driveState = DriveState.STOP;
+                        driveState = DriveState.GET_SECOND_GLYPH;
+
+
                     }
                     break;
 
@@ -156,45 +229,80 @@ public class RedCloseMGAuton extends LinearOpMode {
                         targetEnc = ((robot.driveTrain.getLeftCurrentPosition()+robot.driveTrain.getRightCurrentPosition())/2.0)
                                 -startEnc;
                         robot.driveTrain.setDrive(DriveTrain.Drive.STOP_RESET);
-                        fourBarState = FourBarState.OUTTAKE;
                         robot.driveTrain.stop();
                         driveState = DriveState.DRIVE_BACK_TO_CRYPTO;
+                        fourBarState = FourBarState.OUTTAKE;
                     }
                     break;
                 }
                 case DRIVE_BACK_TO_CRYPTO:
                 {
-                    if(robot.driveTrain.setMoveDistEnc(350-(int)targetEnc))
+                    if(robot.driveTrain.setMoveDistEnc(360-(int)targetEnc))
                     {
                         driveState = firstDState;
                     }
                     break;
                 }
+                case ROTATE_DEG_2:
+                {
+
+                    if(robot.driveTrain.rotateDeg(angle))
+                    {
+                        driveState = DriveState.GET_FIRST_GLYPH;
+                        fourBarState = FourBarState.INTAKE;
+
+                    }
+                    break;
+
+                }
                 case LEFT:
                 {
-                    if(robot.driveTrain.rotateDeg(-165))
+                    if(robot.driveTrain.rotateDeg(angle))
                     {
                         wheelState = WheelState.SPIT_GLYPHS;
                         driveState = DriveState.STOP;
+                        firstDState = DriveState.RIGHT;
+                        if(!driveState.equals(DriveState.RIGHT)) {
+                            driveState = DriveState.RIGHT;
+                            angle = -165;
+                        }
+                        else if(driveState.equals(DriveState.RIGHT))
+                            driveState = DriveState.STOP;
                     }
                     break;
                 }
                 case RIGHT:
                 {
-                    if(robot.driveTrain.rotateDeg(165))
+                    if(robot.driveTrain.rotateDeg(angle))
                     {
                         wheelState = WheelState.SPIT_GLYPHS;
                         driveState = DriveState.STOP;
+                        firstDState = DriveState.LEFT;
+                        if(!driveState.equals(DriveState.LEFT))
+                        {
+                            driveState = DriveState.LEFT;
+                            angle = 165;
+                        }
+                        else if(driveState.equals(DriveState.LEFT))
+                            driveState = DriveState.STOP;
+
                     }
                     break;
 
                 }
                 case CENTER:
                 {
-                    if(robot.driveTrain.rotateDeg(-179))
+                    if(robot.driveTrain.rotateDeg(angle))
                     {
                         wheelState = WheelState.SPIT_GLYPHS;
                         driveState = DriveState.STOP;
+                        if(!driveState.equals(DriveState.RIGHT))
+                        {
+                            driveState = DriveState.RIGHT;
+                            angle = -165;
+                        }
+                        else if(driveState.equals(DriveState.RIGHT))
+                            driveState = DriveState.STOP;
                     }
                     break;
                 }
@@ -248,6 +356,7 @@ public class RedCloseMGAuton extends LinearOpMode {
                                 }
                                 else
                                 {
+                                    startTime = 0;
                                     wheelState = WheelState.STOP;
                                 }
                             } else {
@@ -265,10 +374,26 @@ public class RedCloseMGAuton extends LinearOpMode {
                 }
                 case SPIT_GLYPHS:
                 {
-                    robot.wheels.fullOuttake();
-                    if(robot.wheels.glyphDist()>8)
+                    robot.wheels.halfOuttake();
+                    if(robot.wheels.glyphDist()>6)
                     {
-                        wheelState = WheelState.STOP;
+                        if(startTime == 0)
+                            startTime = timer.milliseconds();
+                        if(timer.milliseconds()-startTime<1000)
+                        {
+                            robot.wheels.intakeLeft();
+                            robot.wheels.intakeRight();
+                        }
+                        else if(robot.wheels.glyphDist()<9){
+
+                            robot.wheels.fullOuttake();
+
+                        }
+                        else
+                        {
+                            wheelState = WheelState.STOP;
+                            driveState = DriveState.ROTATE_DEG_2;
+                        }
                     }
                     break;
                 }
@@ -289,7 +414,7 @@ public class RedCloseMGAuton extends LinearOpMode {
 
                 case OUTTAKE:
                 {
-                    robot.bar4.setMoveHeight(7);
+                    robot.bar4.setMoveHeight(8);
                     break;
                 }
                 case STOP:
@@ -300,8 +425,11 @@ public class RedCloseMGAuton extends LinearOpMode {
             }
 
 
-
-
+            telemetry.addData("time", timer.milliseconds());
+            telemetry.addData("start time", startTime);
+            telemetry.addData("diff", (timer.milliseconds()-startTime));
+            telemetry.addData("wheel state: ",wheelState.toString());
+            telemetry.addData("wheel dist", robot.wheels.glyphDist());
             telemetry.update();
 
         }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/JewlArmie.java
@@ -0,0 +1,168 @@
+package org.firstinspires.ftc.teamcode.Tests;
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.Subsystems.FourBar;
+import org.firstinspires.ftc.teamcode.Subsystems.JewelArm;
+import org.firstinspires.ftc.teamcode.Subsystems.Robot;
+import org.firstinspires.ftc.teamcode.Util.Gyro;
+import org.firstinspires.ftc.teamcode.Util.VisionUtil;
+import org.firstinspires.ftc.teamcode.Util.*;
+
+/**
+ * Created by Sumanth Kondapalli on 11/16/2017.
+ */
+@Autonomous(name = "red jewel test")
+public class JewlArmie extends LinearOpMode {
+
+    Robot robot = new Robot();
+    Gyro gyro = new Gyro();
+    ElapsedTime timer = new ElapsedTime();
+    RobotConstants constant = new RobotConstants();
+    VisionUtil vision = new VisionUtil(this);
+    RelicRecoveryVuMark reading;
+    double startTime = 0;
+    String tester = "";
+    VisionUtil visionUtil = new VisionUtil(this);
+    double angle;
+
+    double startEnc;
+    double targetEnc;
+    enum DriveState
+    {
+        HIT_JEWEL,
+        DRIVE_TO_POS,
+        ROTATE_TO_GLYPH_PIT,
+        GET_FIRST_GLYPH,
+        GET_SECOND_GLYPH,
+        DRIVE_BACK_TO_CRYPTO,
+        LEFT,
+        RIGHT,
+        CENTER,
+        ROTATE_DEG_2,
+        STOP
+    }
+
+    enum JewelState
+    {
+        RED,
+        BLUE,
+        NONE,
+    }
+
+
+    int intkaeCounter = 0;
+    boolean swap = true;
+    enum WheelState
+    {
+        SPAZ_INTAKE,
+        FULL_INTAKE_DOSGLYPHY,
+        SPIT_GLYPHS,
+        STOP
+    }
+
+
+    enum FourBarState
+    {
+        INTAKE,
+        OUTTAKE,
+        STOP
+    }
+
+    enum ArmState
+    {
+        ARM_UP,
+        ARM_DOWN
+    }
+
+
+
+    WheelState wheelState = WheelState.STOP;
+    DriveState driveState = DriveState.DRIVE_TO_POS;
+    DriveState firstDState = DriveState.CENTER;
+    FourBarState fourBarState = FourBarState.STOP;
+    JewelState jewelState = JewelState.NONE;
+    ArmState armState = ArmState.ARM_DOWN;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        AutoTransitioner.transitionOnStop(this,"MainTeleNoPID");
+
+        gyro.initGyro(hardwareMap);
+        robot.init(hardwareMap, this, gyro);
+        reading = vision.readGraph(hardwareMap);
+
+        reading = vision.readGraph2(hardwareMap);
+        telemetry.addData("vision", reading);
+
+        switch(reading)
+        {
+            case LEFT:
+            {
+                firstDState = DriveState.LEFT;
+                angle = 165;
+                break;
+            }
+            case RIGHT:
+            {
+                firstDState = DriveState.RIGHT;
+                angle = -165;
+                break;
+
+            }
+            case CENTER:
+            {
+                firstDState = DriveState.CENTER;
+                angle = 180;
+                break;
+            }
+            default:
+            {
+                firstDState = DriveState.CENTER;
+            }
+        }
+
+        waitForStart();
+
+
+
+        if (opModeIsActive()) {
+            robot.jewelArm.arm2Down();
+            robot.wheels.latch();
+
+
+            sleep(1000);
+            if (String.valueOf(robot.jewelArm.getColor2()).equals("RED")) {
+                telemetry.addData(String.valueOf(robot.jewelArm.getColor2()), "00");
+                telemetry.update();
+
+                robot.driveTrain.setMoveDist(4);
+
+            } else if (String.valueOf(robot.jewelArm.getColor2()).equals("BLUE")) {
+
+                telemetry.addData(String.valueOf(robot.jewelArm.getColor2()), "00");
+                telemetry.update();
+
+                robot.driveTrain.setMoveDist(-4);
+
+            }
+//
+            robot.jewelArm.armMid();
+            robot.jewelArm.arm2Mid();
+        }
+        while(opModeIsActive())
+        {
+           telemetry.addData("this is insanely stupid", "i ahte life");
+           telemetry.update();
+
+        }
+
+    }
+}
